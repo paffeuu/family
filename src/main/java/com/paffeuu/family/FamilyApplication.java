5b7715cc663a328e27ea7d5ac47f5304bbc5bcc6
Paweł Kowański <pawel.kowanski@hotmail.com>
2018-10-18 19:27:08 +0200

    initial commit

---
+++ src/main/java/com/paffeuu/family/FamilyApplication.java
@@ -0,0 +1,12 @@
+package com.paffeuu.family;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilyApplication.class, args);
+	}
+}
